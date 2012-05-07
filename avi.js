3fc46470f76e2cdb78f6cccc78231ffd54390d0b
Sebmaster <sebmaster16@gmail.com>
2012-05-07 11:04:57 +0200

    Since AVI.js changed so much: Major rework.
    I also included BlobBuilder.js and FileSaver.js. BlobBuilder.js shouldn't be necessary as soon as it gets standardized (or I switch avi.js to the blob constructor).
    
    FileSaver.js will be necessary for a longer time though, since the working draft just started.

--- avi.js
+++ avi.js
@@ -18,7 +18,9 @@
 	 * @param {Array.<number>} arr 
 	 */
 	Buffer.prototype.appendArray = function(arr) {
-		this.buffer.push.apply(this.buffer, arr);
+		for (var i=0; i < arr.length; ++i) {
+			this.buffer.push(arr[i]);
+		}
 	};
 	
 	/**
@@ -43,21 +45,6 @@
 	
 	/**
 	 * @param {number} idx
-	 * @param {number} num 
-	 */
-	Buffer.prototype.writeLong = function(idx, num) {
-		this.buffer[idx] = 0;
-		this.buffer[idx + 1] = 0;
-		this.buffer[idx + 2] = 0;
-		this.buffer[idx + 3] = 0;
-		this.buffer[idx + 4] = num & 255;
-		this.buffer[idx + 5] = (num >> 8) & 255;
-		this.buffer[idx + 6] = (num >> 16) & 255;
-		this.buffer[idx + 7] = (num >> 24) & 255;
-	};
-	
-	/**
-	 * @param {number} idx
 	 * @param {string} str 
 	 */
 	Buffer.prototype.writeString = function(idx, str) {
@@ -71,13 +58,12 @@
 	 * @param {Buffer} buf 
 	 */
 	Buffer.prototype.writeBuffer = function(idx, buf) {
-		for (var i=0; i < buf.length; ++i) {
-			this.buffer[idx + i] = buf.buffer[i];
+		var len = buf.length;
+		var myBuf = this.buffer;
+		var remBuf = buf.buffer;
+		for (var i=0; i < len;) {
+			myBuf[idx++] = remBuf[i++];
 		}
-	};
-	
-	Buffer.prototype.toString = function() {
-		return String.fromCharCode.apply(null, this.buffer);
 	};
 	
 	/**
@@ -88,12 +74,17 @@
 		this.id = id;
 		this.data = new Buffer();
 	};
-	
+
 	Chunk.prototype.getBuffer = function() {
 		var buffer = new Buffer();
 		buffer.writeString(0, this.id);
-		buffer.writeInt(4, this.data.length);
 		buffer.writeBuffer(8, this.data);
+		if (this.data.length % 2 == 0) {
+			buffer.writeInt(4, this.data.length);
+		} else {
+			buffer.writeInt(4, this.data.length + 1);
+			buffer.appendArray([0]);
+		}
 		return buffer;
 	};
 	
@@ -127,9 +118,8 @@
 	 */
 	function AVIJS() {
 		this.settings = {
-			width: 5,
-			height: 5,
-			framesPerSecond: 20
+			width: 0,
+			height: 0
 		};
 		
 		this.streams = [];
@@ -139,7 +129,7 @@
 		var buffer = new Buffer();
 		buffer.writeString(0, 'RIFF');
 		buffer.writeString(8, 'AVI ');
-		var len = 0;
+		var len = 4;
 		var frames = 0;
 		for (var i=0; i < this.streams.length; ++i) {
 			frames += this.streams[i].frames.length;
@@ -149,7 +139,7 @@
 		var avih = new Chunk('avih');
 		avih.data.writeInt(0, 66665);
 		avih.data.writeInt(4, 0); // MaxBytesPerSec
-		avih.data.writeInt(8, 1); // Padding (in bytes)
+		avih.data.writeInt(8, 2); // Padding (in bytes)
 		avih.data.writeInt(12, 0); // Flags
 		avih.data.writeInt(16, frames); // Total Frames
 		avih.data.writeInt(20, 0); // Initial Frames
@@ -167,20 +157,22 @@
 			hdrl.elements.push(this.streams[i].getHeaderBuffer());
 		}
 		var hdrlBuf = hdrl.getBuffer();
-		buffer.writeBuffer(12, hdrlBuf);
+		buffer.writeBuffer(8 + len, hdrlBuf);
 		len += hdrlBuf.length;
 		
 		var movi = new List('movi');
 		for (var i=0; i < this.streams.length; ++i) {
-			movi.elements.push(this.streams[i].getDataBuffer(i));
+			movi.elements.push.apply(movi.elements, this.streams[i].getDataBuffer(i));
 		}
 		
 		var moviBuf = movi.getBuffer();
-		buffer.writeBuffer(12 + len, moviBuf);
+		buffer.writeBuffer(8 + len, moviBuf);
 		len += moviBuf.length;
 		
 		buffer.writeInt(4, len);
-		return buffer.toString();
+		var blob = new BlobBuilder();
+		blob.append((new Uint8Array(buffer.buffer)).buffer);
+		return blob.getBlob('video/avi');
 	};
 	
 	/**
@@ -194,10 +186,10 @@
 		this.frames = [];
 	};
 	
-	AVIJS.Stream.prototype.addFrame = function(imgData) {
+	AVIJS.Stream.prototype.addRGBAFrame = function(imgData) {
 		var frame = [];
 		for (var i=0; i < imgData.length; i += 4) {
-			frame.push(imgData[i], imgData[i+1], imgData[i+2],0);
+			frame.push(imgData[i+1], imgData[i+2], imgData[i]);
 		}
 		this.frames.push(frame);
 	};
@@ -206,7 +198,7 @@
 		var list = new List('strl');
 		var strh = new Chunk('strh');
 		strh.data.writeString(0, 'vids'); // fourCC
-		strh.data.writeInt(4, 0); // Uncompressed
+		strh.data.writeString(4, 'DIB '); // Uncompressed
 		strh.data.writeInt(8, 0); // Flags
 		strh.data.writeShort(12, 1); // Priority
 		strh.data.writeShort(14, 0); // Language
@@ -214,38 +206,41 @@
 		strh.data.writeInt(20, 1); // Scale
 		strh.data.writeInt(24, this.fps); // Rate
 		strh.data.writeInt(28, 0); // Startdelay
-		strh.data.writeInt(32, 0); // Length
-		strh.data.writeInt(36, 0); // suggested buffer size
+		strh.data.writeInt(32, this.frames.length); // Length
+		strh.data.writeInt(36, this.width * this.height * 3); // suggested buffer size
 		strh.data.writeInt(40, -1); // quality
 		strh.data.writeInt(44, 0); // sampleSize
-		strh.data.writeInt(48, 0); // Rect left
-		strh.data.writeInt(52, 0); // Rect top
-		strh.data.writeInt(56, this.width); // Rect width
-		strh.data.writeInt(60, this.height); // Rect height
+		strh.data.writeShort(48, 0); // Rect left
+		strh.data.writeShort(50, 0); // Rect top
+		strh.data.writeShort(52, this.width); // Rect width
+		strh.data.writeShort(54, this.height); // Rect height
 		list.elements.push(strh);
 		
 		var strf = new Chunk('strf');
-		strf.data.writeLong(0, this.width); // width
-		strf.data.writeLong(8, this.height); // height
-		strf.data.writeShort(16, 1); // planes
-		strf.data.writeShort(18, 32); // bits per pixel
-		strf.data.writeInt(20, 0); // compression
-		strf.data.writeInt(24, 0); // image size
-		strf.data.writeLong(28, 0); // x pixels per meter
-		strf.data.writeLong(36, 0); // y pixels per meter
-		strf.data.writeInt(44, 0); // colortable used
-		strf.data.writeInt(48, 0); // colortable important
+		strf.data.writeInt(0, 40); // struct size
+		strf.data.writeInt(4, this.width); // width
+		strf.data.writeInt(8, -this.height); // height
+		strf.data.writeShort(12, 1); // planes
+		strf.data.writeShort(14, 24); // bits per pixel
+		strf.data.writeInt(16, 0); // compression
+		strf.data.writeInt(20, 0); // image size
+		strf.data.writeInt(24, 0); // x pixels per meter
+		strf.data.writeInt(28, 0); // y pixels per meter
+		strf.data.writeInt(32, 0); // colortable used
+		strf.data.writeInt(36, 0); // colortable important
 		list.elements.push(strf);
 		
 		return list;
 	};
 	
 	AVIJS.Stream.prototype.getDataBuffer = function(idx) {
-		var chk = new Chunk((idx < 10 ? '0' + idx : idx) + 'db');
+		var chunks = [];
 		for (var i=0; i < this.frames.length; ++i) {
+			var chk = new Chunk((idx < 10 ? '0' + idx : idx) + 'db');
 			chk.data.appendArray(this.frames[i]);
+			chunks.push(chk);
 		}
-		return chk;
+		return chunks;
 	};
 	
 	window['AVIJS'] = AVIJS;
